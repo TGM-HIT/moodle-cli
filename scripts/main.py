537a37a7aa269de9bf06d275047bcebf879eb8c8
Clemens Koza <clemens.koza@gmx.at>
2025-07-19 19:55:03 +0200

    support file upload

--- scripts/main.py
+++ scripts/main.py
@@ -3,30 +3,75 @@ import sys
 
 from bs4 import BeautifulSoup
 from moodle import Moodle
+import requests
 import typst
 
-url = 'http://localhost:8000/webservice/rest/server.php'
-token = '2e884e3588b06e1c8d0b43150eff366e'
+base_url = 'http://localhost:8000'
+url = f'{base_url}/webservice/rest/server.php'
+token = '3b29d57634d4a547d48f52638b374a30'
+
 
 def process(filename: str):
     metadata = json.loads(typst.query(filename, '<frontmatter>', field="value", one=True))
     soup = BeautifulSoup(typst.compile(filename, format='html'), 'html.parser')
 
-    return { **metadata, 'body': soup.body.decode_contents() }
+    return metadata, soup.body.decode_contents()
 
-def upload(data: dict, url: str, token: str):
-    if data['mod'] != 'page':
-        raise ValueError(f"Unsupported module type: {data['mod']}")
+
+def upload_page(url: str, token: str, *, cmid: int, intro: dict, page: dict):
     moodle = Moodle(url, token)
-    result = moodle('local_modcontentservice_update_page_content', cmid=data['cmid'], body=data['body'])
+    result = moodle('local_modcontentservice_update_page_content', cmid=cmid, intro=intro, page=page)
     return result
 
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Usage: python main.py <filename>")
-        sys.exit(1)
 
-    filename = sys.argv[1]
-    data = process(filename)
-    result = upload(data, url, token)
+def upload_files(*files, itemid = 0, filepath = '/'):
+    r = requests.post(
+        f'{base_url}/webservice/upload.php',
+        params={
+            'token': token,
+            'itemid': itemid,
+            'filepath': filepath,
+        },
+        files={ f'file_{i}': file for i, file in enumerate(files, start=1) },
+    )
+    r.raise_for_status()
+    return r.json()
+
+if __name__ == '__main__':
+    # if len(sys.argv) < 2:
+    #     print("Usage: python main.py <filename>")
+    #     sys.exit(1)
+
+    # filename = sys.argv[1]
+    # metadata, body = process(filename)
+    # if metadata['mod'] != 'page':
+    #     raise ValueError(f"Unsupported module type: {metadata['mod']}")
+
+    # result = upload_page(url, token,
+    #     cmid=metadata['cmid'],
+    #     intro=dict(text=''),
+    #     page=dict(text=body),
+    # )
+    # print(result)
+
+    result = upload_files(
+        ('super-advocado.jpg', open('/home/clemens/Pictures/super-advocado.jpg', 'rb')),
+        # ('Apothecary.jpg', open('/home/clemens/Pictures/WerewolfDarkArts/cropped/Apothecary.jpg', 'rb')),
+    )
+    itemid = result[0]['itemid']
+    # print(result)
+
+    result = upload_page(url, token,
+        cmid=2,
+        intro=dict(
+            text='This is the intro',
+            # format=1,
+            # itemid=-1,
+        ),
+        page=dict(
+            text='<p><img class="img-fluid" src="@@PLUGINFILE@@/super-advocado.jpg" alt="advocado" width="1024" height="1024"></p>',
+            # format=1,
+            itemid=itemid,
+        ),
+    )
     print(result)
