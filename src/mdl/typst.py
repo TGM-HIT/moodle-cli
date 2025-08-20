03f9709a8e28de6fb4909f1110a732212add3109
Clemens Koza <ckoza@tgm.ac.at>
2025-08-20 23:46:53 +0200

    use environment to configure Typst root

--- src/mdl/typst.py
+++ src/mdl/typst.py
@@ -1,20 +1,21 @@
-import json
 from pathlib import Path
+import json
+import os
 
 from bs4 import BeautifulSoup
 import typst
 
 
 def frontmatter(filename: Path):
-    return json.loads(typst.query(filename, '<frontmatter>', field="value", one=True))
+    return json.loads(typst.query(filename, '<frontmatter>', root=os.getenv('TYPST_ROOT'), field="value", one=True))
 
 
 def attachments(filename: Path):
-    return json.loads(typst.query(filename, '<attachments>', field="value"))
+    return json.loads(typst.query(filename, '<attachments>', root=os.getenv('TYPST_ROOT'), field="value"))
 
 
 def body(filename: Path):
-    html = typst.compile(filename, format='html')
+    html = typst.compile(filename, root=os.getenv('TYPST_ROOT'), format='html')
     body = BeautifulSoup(html, 'html.parser').body.decode_contents()
 
     return body
